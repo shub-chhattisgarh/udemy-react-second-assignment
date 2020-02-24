3c2f9a6259590b8bed940882d4bfda9545b8c328
Shubham Nishad <snshusat161@gmail.com>
2020-02-24 22:44:20 +0530

    Created Task 1

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component{
+  
+  state = {
+    input: "",
+  }
+
+  inputHandler = event => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+
+  render(){
+    const style = {
+      padding: '10px',
+      margin: '16px',
+      width: '400px',
+      fontSize: '1.2rem'
+    }
+    return (
+      <div className='App'>
+        <input type='text' value={this.state.input} onChange={this.inputHandler} placeholder='type your text here' style={style}/><br/>
+        <p>The length of the Input is {this.state.input.length}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
